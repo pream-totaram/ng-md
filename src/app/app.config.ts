24c4538b01083fae6a676257f6dd7271e5632a4d
Pream Totaram <ptotaram@fearless.tech>
2024-04-24 09:43:28 -0400

    Reading and rendering markdown from github gist

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { SanitizedHtmlPipe } from './sanitized-html.pipe';
+import { FileService } from './file.service';
+import { provideHttpClient } from '@angular/common/http';
+
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    SanitizedHtmlPipe
+    SanitizedHtmlPipe,
+    FileService,
+    provideHttpClient()
   ]
 };
--- src/app/components/markdown/markdown.component.ts
+++ src/app/components/markdown/markdown.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Marked } from '@ts-stack/markdown';
 import { SanitizedHtmlPipe } from '../../sanitized-html.pipe';
+import { FileService } from '../../file.service';
 
 @Component({
   selector: 'app-markdown',
@@ -10,9 +11,25 @@ import { SanitizedHtmlPipe } from '../../sanitized-html.pipe';
   styleUrl: './markdown.component.scss'
 })
 
-export class MarkdownComponent {
-  markup: string;
-  constructor() {
-    this.markup = Marked.parse("## How are you?");
+export class MarkdownComponent implements OnInit {
+  markup: string = '';
+  constructor(private fileService: FileService) {}
+
+  ngOnInit(): void {
+     this.fileService.getFileContent('https://gist.githubusercontent.com/pream-totaram/a3f17f7d08549f45297aad90315d3a32/raw/78ff4b3c94b1f1bf95ba87474b5c86d6cc55cc11/angular.md').subscribe(content => {
+       this.markup = Marked.parse(content);
+     });
   }
 }
+
+/**
+ *
+
+   export class FileComponent implements OnInit {
+     fileContent: string;
+
+     constructor(private fileService: FileService) {}
+
+     }
+   }
+  */
